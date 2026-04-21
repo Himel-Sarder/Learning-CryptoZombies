d266f22b1d7c6a4450ad22d1bae5f1a604f2a846
Himel Sarder <143216886+Himel-Sarder@users.noreply.github.com>
2026-04-21 13:30:14 +0600

    initial commit

--- Solidity: Beginner to Intermediate Smart Contracts/Making The Zombie Factory/Contracts.sol
+++ Solidity: Beginner to Intermediate Smart Contracts/Making The Zombie Factory/Contracts.sol
@@ -1,6 +1,6 @@
-A contract in blockchain (especially Ethereum) is a piece of code that is stored on the blockchain and automatically executes when certain conditions are met.
+// A contract in blockchain (especially Ethereum) is a piece of code that is stored on the blockchain and automatically executes when certain conditions are met.
 
-In Solidity, a contract is like a blueprint for creating smart applications.
+// In Solidity, a contract is like a blueprint for creating smart applications.
 
 pragma solidity >=0.5.0 <0.6.0;
 
